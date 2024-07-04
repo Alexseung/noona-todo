4f3cb4b7fae9be772fa43d6d18cb322caf16a74b
Seunggeun Yoon <tmdmrs522@gmail.com>
2024-07-04 20:42:48 +0900

    left design

--- main.js
+++ main.js
@@ -8,6 +8,7 @@ let taskOngoing = document.getElementById('task-ongoing');
 let taskDone = document.getElementById('task-done');
 let taskList = [];
 let filterList = []; //기본을 taskList로 잡고 isComplete에따라 새로운 배열에 넣기위한 배열
+let pinnedList = [];
 let currentTab = 'show-all';
 //현재 어떤 tab에 있는지 나타낼 수 있고 해당 tab를 누르면 render()을 다시 실행해서 목록을 보여줌
 showAll.addEventListener('click', () => {
@@ -24,6 +25,51 @@ taskDone.addEventListener('click', () => {
   currentTab = 'task-done';
   filter();
 });
+
+// 즐겨찾기 pin기능
+function pinTask(id) {
+  // taskList, pinnedList 모두에서 해당 요소를 찾아야하기에 변수 두개 선언
+  let taskIndex = taskList.findIndex(task => task.id === id);
+  let pinnedIndex = pinnedList.findIndex(task => task.id === id);
+
+  // taskList에서 찾았을 경우
+  if (taskIndex !== -1) {
+    //findIndex에서 이런식으로 해당 요소 존재여부 파악, 없으면 -1을 반환하기에 !== -1은 있다는 소리
+    taskList[taskIndex].isPinned = !taskList[taskIndex].isPinned;
+
+    // isPinned가 true이면 pinnedList에 추가하고 taskList에서 제거
+    if (taskList[taskIndex].isPinned) {
+      pinnedList.push(taskList[taskIndex]); //pinnedList에 추가하고
+      taskList.splice(taskIndex, 1); //taskList에서를 자르기
+    }
+
+    render();
+    console.log(pinnedList);
+    console.log(taskList);
+    return; //여기에서 꼭 끊어줘야함
+  }
+
+  // pinnedList에서 찾았을 경우
+  if (pinnedIndex !== -1) {
+    pinnedList[pinnedIndex].isPinned = !pinnedList[pinnedIndex].isPinned;
+
+    // isPinned가 false이면 taskList에 추가하고 pinnedList에서 제거
+    if (!pinnedList[pinnedIndex].isPinned) {
+      taskList.push(pinnedList[pinnedIndex]); //taskList에 추가하고
+      pinnedList.splice(pinnedIndex, 1); //pinnedList에서는 제거
+    }
+
+    render();
+    console.log(pinnedList);
+    console.log(taskList);
+    return;
+  }
+
+  // taskList와 pinnedList에서 모두 해당 태스크를 찾지 못한 경우
+  render(); // 그냥 render
+  console.log(pinnedList);
+  console.log(taskList);
+}
 
 function filter() {
   filterList = [];
@@ -61,6 +107,8 @@ function addTask() {
     id: randomIdGenerate(),
     taskContent: taskInput.value,
     isComplete: false,
+    isPinned: false,
+    dueDate: '',
   };
   taskList.push(task);
   console.log(taskList);
@@ -71,9 +119,17 @@ function addTask() {
 function render() {
   let list = [];
   if (currentTab === 'show-all') {
-    list = taskList;
-  } else if (currentTab === 'task-ongoing' || currentTab === 'task-done') {
-    list = filterList;
+    list = [...pinnedList, ...taskList];
+  } else if (currentTab === 'task-ongoing') {
+    list = [
+      ...pinnedList.filter(task => !task.isComplete && task.isPinned),
+      ...taskList.filter(task => !task.isComplete && !task.isPinned),
+    ];
+  } else if (currentTab === 'task-done') {
+    list = [
+      ...pinnedList.filter(task => task.isComplete),
+      ...taskList.filter(task => task.isComplete),
+    ];
   }
   let resultHTML = '';
   for (let i = 0; i < list.length; i++) {
@@ -81,17 +137,24 @@ function render() {
       resultHTML += `<div class="task task-done">
               <div>${list[i].taskContent} </div>
               <div>
-                <i class="fa-solid fa-rotate-left" onClick="toggleBtn('${list[i].id}')"></i>
+                <i class="fa-solid fa-rotate-left" onClick="checkBtn('${list[i].id}')"></i>
                 <i class="fa-solid fa-trash" onClick="deleteBtn('${list[i].id}')"></i>
               </div>
             </div>`;
     } else {
-      resultHTML += `<div class="task" onclick='setDate()'>
+      resultHTML += `<div class="task ${list[i].isPinned ? 'pinned-task' : ''}">
                 <div>${list[i].taskContent}</div>
                 <div>
+                  <i class="fa-solid fa-thumbtack" onClick="pinTask('${
+                    list[i].id
+                  }')"></i>
                   <i class="fa-solid fa-calendar-days" onClick='dueDate()'></i>
-                  <i class="fa-solid fa-check" onClick="toggleBtn('${list[i].id}')"></i>
-                  <i class="fa-solid fa-trash" onClick="deleteBtn('${list[i].id}')"></i>
+                  <i class="fa-solid fa-check" onClick="checkBtn('${
+                    list[i].id
+                  }')"></i>
+                  <i class="fa-solid fa-trash" onClick="deleteBtn('${
+                    list[i].id
+                  }')"></i>
                 </div>
               </div>`;
     }
@@ -100,22 +163,31 @@ function render() {
 }
 
 // 마감일 지정
-function dueDate() {
+function dueDate(id) {
   let myModal = new bootstrap.Modal(document.getElementById('exampleModal'), {
     keyboard: false,
   });
   myModal.show();
 }
 
-function saveDate() {
-  // let selectedDate = document.getElementById('date-input').value;  실행방법 찾는중
+function saveDate(id) {
   let myModal = bootstrap.Modal.getInstance(
     document.getElementById('exampleModal')
   );
   myModal.hide();
+  render();
 }
 
-function toggleBtn(id) {
+//Check toggle
+function checkBtn(id) {
+  let foundPinned = false;
+  for (let i = 0; i < pinnedList.length; i++) {
+    if (pinnedList[i].id === id) {
+      pinnedList[i].isComplete = !pinnedList[i].isComplete;
+      foundPinned = true;
+      break;
+    }
+  }
   for (i = 0; i < taskList.length; i++) {
     if (taskList[i].id == id) {
       taskList[i].isComplete = !taskList[i].isComplete;
@@ -123,11 +195,13 @@ function toggleBtn(id) {
     }
   }
   if (currentTab === 'task-ongoing') {
-    filterList = taskList.filter(task => !task.isComplete); //false가 filterList에 남아있게
+    (filterList = pinnedList.filter(task => !task.isComplete && task.isPinned)),
+      taskList.filter(task => !task.isComplete); //false가 filterList에 남아있게
   } else if (currentTab === 'task-done') {
-    filterList = taskList.filter(task => task.isComplete); //true가 filterList에 남아있게
+    (filterList = pinnedList.filter(task => task.isComplete && !task.isPinned)),
+      taskList.filter(task => task.isComplete); //true가 filterList에 남아있게
   }
-  console.log(taskList);
+  console.log(filterList);
   render();
 }
 
@@ -135,6 +209,8 @@ function toggleBtn(id) {
 function deleteBtn(id) {
   taskList = taskList.filter(task => task.id !== id);
   console.log(taskList);
+  pinnedList = pinnedList.filter(task => task.id !== id);
+  console.log(pinnedList);
   if (currentTab === 'show-all') {
     render();
   } else {
